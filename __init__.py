ea70bf87cdc679b5c0b61179cce25d3792aa48e5
yuan-SiO2 <861745284@qq.com>
2026-06-04 08:16:11 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ from .nodes import (
 from .yuan_nodes.yuan_txt_splitter import NODE_CLASS_MAPPINGS as YUAN_TXT_MAPPINGS
 from .yuan_nodes.yuan_txt_splitter import NODE_DISPLAY_NAME_MAPPINGS as YUAN_TXT_DISPLAY_MAPPINGS
 
+from .bernini import NODE_CLASS_MAPPINGS as BERNINI_MAPPINGS
+from .bernini import NODE_DISPLAY_NAME_MAPPINGS as BERNINI_DISPLAY_MAPPINGS
+
 NODE_CLASS_MAPPINGS = {
     "QwenVL_ModelLoader": QwenVL模型加载器,
     "QwenVL_ImageInfer": QwenVL图像推理,
     "QwenVL_Unload": QwenVL卸载模型,
     **YUAN_TXT_MAPPINGS,
+    **BERNINI_MAPPINGS,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,6 +23,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "QwenVL_ImageInfer": "Qwen VL 图像推理",
     "QwenVL_Unload": "Qwen VL 卸载模型",
     **YUAN_TXT_DISPLAY_MAPPINGS,
+    **BERNINI_DISPLAY_MAPPINGS,
 }
 
 WEB_DIRECTORY = "./web"
